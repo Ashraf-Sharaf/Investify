0c448a5b78fc9228b658599a2b6112060ad91fa1
Ashraf Sharaf <ashrafsharafcs@gmail.com>
2024-04-28 02:07:30 +0300

    editing-user-roles-table

--- backend/database/migrations/2024_04_27_133552_create_user_roles_table.php
+++ backend/database/migrations/2024_04_27_133552_create_user_roles_table.php
@@ -11,8 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('_user_roles', function (Blueprint $table) {
+        Schema::create('user_roles', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('role_id');
+            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->timestamps();
         });
     }
