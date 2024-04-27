265db11b6950c7b438a3f673701f96063e408b19
Ashraf Sharaf <ashrafsharafcs@gmail.com>
2024-04-27 16:35:16 +0300

    creating-favorites-table

---
+++ backend/database/migrations/2024_04_27_133417_create_favorites_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('favorites', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('investor_id');
+            $table->foreign('investor_id')->references('id')->on('users')->onDelete('cascade');
+            $table->unsignedBigInteger('business_id');
+            $table->foreign('business_id')->references('id')->on('businesses')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('favorites');
+    }
+};
