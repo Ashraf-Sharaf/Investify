7c96949b924d1896d7f9dfb878df499b96f80052
Ashraf Sharaf <ashrafsharafcs@gmail.com>
2024-04-28 03:36:38 +0300

    adding-business-model

--- backend/app/Models/Business.php
+++ backend/app/Models/Business.php
@@ -8,5 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Business extends Model
 {
     use HasFactory;
+    public function owner()
+    {
+        return $this->belongsTo(User::class);
+    }
 
+    public function investor()
+    {
+        return $this->hasOne(User::class);
+    }
 }
