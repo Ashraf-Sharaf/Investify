a6368b06a1a008b7720b6da6c6b70ca70224dff8
Ashraf Sharaf <ashrafsharafcs@gmail.com>
2024-04-28 03:48:04 +0300

    adding-event-model

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -24,9 +24,14 @@ class User extends Authenticatable
         return $this->hasMany(Review::class);
     }
 
-    public function favrites()
+    public function favorites()
     {
-        return $this->hasMany(Review::class);
+        return $this->hasMany(Favorite::class);
+    }
+
+    public function events()
+    {
+        return $this->hasMany(Event::class);
     }
 
 
