69ea22e4d7e4280522e5550c6768d92834b444ff
Ashraf Sharaf <ashrafsharafcs@gmail.com>
2024-04-29 16:32:54 +0300

    implementing-add-business-function

--- backend/app/Http/Controllers/BusinessController.php
+++ backend/app/Http/Controllers/BusinessController.php
@@ -3,8 +3,47 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Business;
 
 class BusinessController extends Controller
 {
-    //
+    public function add_business(Request $request)
+    {
+        $request->validate(([
+            'name' => 'required|string',
+            'industry' => 'required|string',
+            'location' => 'required|string',
+            'description' => 'required|string',
+            'funding_needed' => 'required|numeric',
+            'stake_offered' => 'required|numeric',
+            'valuation' => 'required|numeric',
+
+        ]));
+
+        $entrepreneur = Auth::id();
+
+        if (!$entrepreneur) {
+            return response()->json([
+                'status' => 403,
+                'message' => "Unauthorized"
+            ]);
+        }
+
+        $business = Business::create([
+            'name' => $request->name,
+            'industry' => $request->industry,
+            'location' => $request->location,
+            'description' => $request->description,
+            'funding_needed' => $request->funding_needed,
+            'stake_offered' => $request->stake_offered,
+            'valuation' => $request->valuation,
+            'entrepreneur_id'=>$entrepreneur,
+        ]);
+
+        return response()->json([
+            'status' => 200,
+            'message' => 'Business added successfully'
+        ]);
+    }
 }
