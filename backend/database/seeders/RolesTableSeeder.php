116b0060b995b2695768c3a0e77e866320d74e9f
Ashraf Sharaf <ashrafsharafcs@gmail.com>
2024-04-29 14:30:54 +0300

    adding-user-role-seeder

--- backend/database/seeders/RolesTableSeeder.php
+++ backend/database/seeders/RolesTableSeeder.php
@@ -46,5 +46,19 @@ class RolesTableSeeder extends Seeder
         ]);
 
 
+        UserRole::create([
+            'role_id' => 1,
+            'user_id' => 1
+        ]);
+
+        UserRole::create([
+            'role_id' => 2,
+            'user_id' => 2
+        ]);
+
+        UserRole::create([
+            'role_id' => 3,
+            'user_id' => 3
+        ]);
     }
 }
