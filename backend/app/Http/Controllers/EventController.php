771176b011a7f7e97707d07b9901607344bff681
Ashraf Sharaf <ashrafsharafcs@gmail.com>
2024-04-30 01:43:37 +0300

    creating-add-evnet-function

---
+++ backend/app/Http/Controllers/EventController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Event;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class EventController extends Controller
+{
+    public function create_event(Request $request)
+    {
+        $hostID = Auth::id();
+
+        $request->validate(([
+            'date' => 'required|date',
+            'time' => 'required|time',
+        ]));
+
+        if (!$hostID) {
+            return response()->json([
+                'status' => 403,
+                'message' => "Unauthorized"
+            ]);
+        }
+
+        $event = Event::create([
+            'host_id' => $hostID,
+            'date' => $request->date,
+            'time' => $request->time
+        ]);
+
+        return response()->json([
+            'status' => 200,
+            'message' => 'Evnet added successfully'
+        ]);
+    }
+}
