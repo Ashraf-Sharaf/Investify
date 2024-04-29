812687150db304372ce5976eaaa6dd4676f5c6ce
Ashraf Sharaf <ashrafsharafcs@gmail.com>
2024-04-29 22:23:23 +0300

    creating-add-review-function

--- backend/app/Http/Controllers/ReviewController.php
+++ backend/app/Http/Controllers/ReviewController.php
@@ -2,9 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Review;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ReviewController extends Controller
 {
-    //
+    public function add_review(Request $request){
+        $investor=Auth::id();
+
+        if (!$investor) {
+            return response()->json([
+                'status' => 401,
+                'message' => 'Unauthorized'
+            ]);
+        }
+
+        $review=Review::create([
+            'rating'=>$request->rating,
+            'description'=>$request->description,
+            'investor_id'=>$investor,
+            'business_id'=>$request->business_id
+        ]);
+
+        return response()->json([
+            'status' => 200,
+            'message' => 'Review added successfully'
+        ]);
+    }
 }
