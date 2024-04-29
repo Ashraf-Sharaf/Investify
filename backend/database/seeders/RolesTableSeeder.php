001d4379db6e2bbe050fa1099e400cd8f404498f
Ashraf Sharaf <ashrafsharafcs@gmail.com>
2024-04-29 14:30:16 +0300

    adding-roles-table-seeder

---
+++ backend/database/seeders/RolesTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\UserRole;
+use App\Models\Role;
+use App\Models\User;
+
+class RolesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Role::create([
+            'name' => 'admin',
+        ]);
+
+        Role::create([
+            'name' => 'investor',
+        ]);
+
+        Role::create([
+            'name' => 'entrepreneur',
+        ]);
+
+    }
+}
