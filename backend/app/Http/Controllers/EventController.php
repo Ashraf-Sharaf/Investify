f22ac45f599fdcc37413c7e48dd0e416b91c2b6a
Ashraf Sharaf <ashrafsharafcs@gmail.com>
2024-04-30 02:18:39 +0300

    adding-edit-event-function

--- backend/app/Http/Controllers/EventController.php
+++ backend/app/Http/Controllers/EventController.php
@@ -32,7 +32,38 @@ class EventController extends Controller
 
         return response()->json([
             'status' => 200,
-            'message' => 'Evnet added successfully'
+            'message' => 'Event added successfully'
+        ]);
+    }
+
+    public function edit_event(Request $request)
+    {
+        $hostID = Auth::id();
+
+        $data = $request->validate(([
+            'date' => 'date',
+            'time' => 'date_format:H:i',
+        ]));
+
+        if (!$hostID) {
+            return response()->json([
+                'status' => 403,
+                'message' => "Unauthorized"
+            ]);
+        }
+        $eventID = $request->event_id;
+        $event = Event::findORFail($eventID);
+
+        foreach ($data as $attribute => $value) {
+            if (!is_null($value)) {
+                $event->$attribute = $value;
+            }
+        }
+        $event->save();
+
+        return response()->json([
+            'status' => 200,
+            'message' => 'Event added successfully'
         ]);
     }
 }
