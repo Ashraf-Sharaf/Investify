8bee8748c7ad42e4ffeacfbcff4696003702f22f
Ashraf Sharaf <ashrafsharafcs@gmail.com>
2024-04-30 20:59:25 +0300

    backend-creating-edit-meeting-function

--- backend/app/Http/Controllers/MeetingController.php
+++ backend/app/Http/Controllers/MeetingController.php
@@ -10,11 +10,11 @@ class MeetingController extends Controller
     public function create_meeting(Request $request)
     {
 
-        $request->validate(([
+        $request->validate([
             'date' => 'required|date',
             'start_time' => 'required|date_format:H:i',
             'end_time' => 'required|date_format:H:i'
-        ]));
+        ]);
 
         $meeting = Meeting::create([
             'date' => $request->date,
@@ -28,5 +28,30 @@ class MeetingController extends Controller
             'message' => 'Meeting created succeffuly',
             'meeting_details' => $meeting
         ]);
+    }
+
+    public function edit_meeting(Request $request)
+    {
+        $data = $request->validate([
+            'date' => 'date',
+            'start_time' => 'date_format:H:i',
+            'end_time' => 'date_format:H:i'
+        ]);
+
+        $meetingID=$request->meeting_id;
+        $meeting=Meeting::findOrFail($meetingID);
+
+        foreach ($data as $attribute => $value) {
+            if (!is_null($value)) {
+                $meeting->$attribute = $value;
+            }
+        }
+        $meeting->save();
+
+        return response()->json([
+            'status' => 200,
+            'message' => 'Meeting Edited successfully'
+        ]);
+
     }
 }
