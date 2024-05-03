22da6764a877901f4119c87d10521654cd04c95a
Ashraf Sharaf <ashrafsharafcs@gmail.com>
2024-05-03 22:24:23 +0300

    frontend-adding-signup-inputs

--- frontend/src/pages/Auth/components/login.jsx
+++ frontend/src/pages/Auth/components/login.jsx
@@ -7,10 +7,18 @@ function Login() {
         <div className="flex center column greeting between">
           <h1>Welcome Back</h1>
           <p>Register with your personal details to use all of site featuers</p>
-          <button >Sign up</button>
+          <button className="auth-button">Log in</button>
         </div>
       </div>
-      <div className="flex column center signup-form">inputs</div>
+      <div className="flex column center signup-section">
+        <div className="flex column between center  signup-form">
+        <h1>Sign Up</h1>
+        <input className="auth-inputs" placeholder="E-mail"></input>
+        <input className="auth-inputs" placeholder="Password"></input>
+        <input className="auth-inputs" placeholder="Confirm Password"></input>
+        <button className="auth-button">Sign up</button>
+        </div>
+      </div>
     </div>
   );
 }
