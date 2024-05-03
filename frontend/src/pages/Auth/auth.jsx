c00794d9bb326ac79f0317db536dc06fb2e897f7
Ashraf Sharaf <ashrafsharafcs@gmail.com>
2024-05-03 21:50:05 +0300

    frontend-creating-login-page

--- frontend/src/pages/Auth/auth.jsx
+++ frontend/src/pages/Auth/auth.jsx
@@ -1,7 +1,10 @@
+import Login from "./components/login";
+import "./auth.css";
+
 function Auth() {
     return (
       <div>
-        
+        <Login/>
       </div>
     );
   }
---
+++ frontend/src/pages/Auth/components/login.jsx
@@ -0,0 +1,18 @@
+
+
+function Login() {
+  return (
+    <div className="flex bg-secondary main-container">
+      <div className="flex column center greeting-section">
+        <div className="flex center column greeting between">
+          <h1>Welcome Back</h1>
+          <p>Register with your personal details to use all of site featuers</p>
+          <button >Sign up</button>
+        </div>
+      </div>
+      <div className="flex column center signup-form">inputs</div>
+    </div>
+  );
+}
+
+export default Login;
