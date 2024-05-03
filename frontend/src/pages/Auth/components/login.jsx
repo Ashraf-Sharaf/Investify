f2ab94df19d1aa3e0792855c497f0fb8c52f4c12
Ashraf Sharaf <ashrafsharafcs@gmail.com>
2024-05-03 23:11:28 +0300

    frontend-addedlogin-form

--- frontend/src/pages/Auth/components/login.jsx
+++ frontend/src/pages/Auth/components/login.jsx
@@ -1,15 +1,15 @@
 function Login({ onToggle }) {
   return (
     <div className="flex bg-secondary main-container">
-      <div className="flex column center greeting-section">
+      <div className="flex column center signup greeting-section">
         <div className="flex center column greeting between">
-          <h1>Welcome Back</h1>
+          <h1>Welcome To Investify</h1>
           <p>Register with your personal details to use all of site featuers</p>
           <button className="auth-button" onClick={() => onToggle()}>Log in</button>
         </div>
       </div>
-      <div className="flex column center signup-section">
-        <div className="flex column between center  signup-form">
+      <div className="flex column center auth-section">
+        <div className="flex column between center  auth-form">
           <h1>Sign Up</h1>
           <input className="auth-inputs" placeholder="E-mail"></input>
           <input className="auth-inputs" placeholder="Password"></input>
--- frontend/src/pages/Auth/components/signup.jsx
+++ frontend/src/pages/Auth/components/signup.jsx
@@ -1,7 +1,24 @@
-function Signup() {
+function Signup({onToggle}) {
     return (
       <div >
-
+    <div className="flex bg-secondary main-container">
+ 
+      <div className="flex column center auth-section">
+        <div className="flex column between center  auth-form">
+          <h1>Log in</h1>
+          <input className="auth-inputs" placeholder="E-mail"></input>
+          <input className="auth-inputs" placeholder="Password"></input>
+          <button className="auth-button" >Log in</button>
+        </div>
+      </div>
+      <div className="flex column center login greeting-section">
+        <div className="flex center column greeting between">
+          <h1>Welcome Back</h1>
+          <p>Log in with your personal details to use all of site featuers</p>
+          <button className="auth-button" onClick={() => onToggle()}>Sign up</button>
+        </div>
+      </div>
+    </div>
       </div>
     );
   }
