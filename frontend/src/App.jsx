2fdcdc77c730854768357725c86157c75b46cb10
Ashraf Sharaf <ashrafsharafcs@gmail.com>
2024-05-03 15:41:18 +0300

    frontend-importing-styling-files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Auth from "./pages/Auth/auth";
-
+import"./styles/colors.css";
+import"./styles/index.css";
+import"./styles/utilities.css";
 
 function App() {
   return (
