bafc4576b409a70b7796d2895e778b415b68996e
Ashraf Sharaf <ashrafsharafcs@gmail.com>
2024-05-04 21:08:41 +0300

    frontend-editing-hero-section-color-and-adding-navigation

--- frontend/src/pages/Landing/components/Header.jsx
+++ frontend/src/pages/Landing/components/Header.jsx
@@ -1,11 +1,15 @@
+import { useNavigate } from "react-router-dom";
+
 function Header(){
+    const navigate = useNavigate();
+    
     return <div className="landing-nav flex align padding-20 between">
         <div className="landing-nav-logo ">
             <img className="logo " src="/images/Investify.png" alt="logo"></img>
         </div>
         <div className="flex gap-10">
-        <button className="login-nav-button" >Log in</button>
-        <button className="signup-nav-button" >Sign up</button>
+        <button className="login-nav-button" onClick={()=>{navigate('/auth')}}>Log in</button>
+        <button className="signup-nav-button" onClick={()=>{navigate('/auth')}}>Sign up</button>
         </div>
 
     </div>
