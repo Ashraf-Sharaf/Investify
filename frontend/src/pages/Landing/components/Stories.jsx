156488f4ccac62d79474ef899c1034e5f8245741
Ashraf Sharaf <ashrafsharafcs@gmail.com>
2024-05-04 17:51:06 +0300

    frontend-creating-success-stories-section-in-landing-page

---
+++ frontend/src/pages/Landing/components/Stories.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+const data = [
+  { name: "user one", review: "Love how this platform is good" },
+  { name: "user two", review: "Love how this platform is good" },
+  { name: "user three", review: "Love how this platform is good" },
+];
+function Stories() {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+  return (
+    <div className="story-section flex column center around">
+      <h1>Success Stories</h1>
+      <Slider {...settings}>
+        {data.map((d)=>(
+        <div key={d.name} className="story-card flex center column gap-20">
+          <h2>{d.name}</h2>
+          <p>{d.review}</p>
+        </div>
+        ))}
+      </Slider>
+    </div>
+  );
+}
+export default Stories;
--- frontend/src/pages/Landing/landing.jsx
+++ frontend/src/pages/Landing/landing.jsx
@@ -2,12 +2,14 @@ import "./landing.css";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Analytics from "./components/Analytics";
+import Stories from "./components/Stories";
 
 function Landing(){
     return <div>
     <Header/>
     <Hero/>
     <Analytics/>
+    <Stories/>
     </div>
 }
 export default Landing;
