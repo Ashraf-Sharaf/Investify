58d7205ab76344a2eaa8cd0ba55597cf672e974a
Ashraf Sharaf <ashrafsharafcs@gmail.com>
2024-05-04 15:11:14 +0300

    frontend-adding-analyitics-section-to-the-landing-page

--- frontend/src/pages/Landing/components/Analytics.jsx
+++ frontend/src/pages/Landing/components/Analytics.jsx
@@ -1,6 +1,22 @@
-function Analytics(){
-    return <div className="hero-section flex center column ">
-     
+function Analytics() {
+  return (
+    <div className="analytics-section flex column around center">
+      <h1 className="padding-10">Quick Facts</h1>
+      <div className="analytics-cards flex around center">
+        <div className="analytics-card flex center column around">
+          <h1>300+</h1>
+          <p>Startups funded</p>
+        </div>
+        <div className="analytics-card flex center column around">
+          <h1>300+</h1>
+          <p>Startups funded</p>
+        </div>
+        <div className="analytics-card flex center column around">
+          <h1>300+</h1>
+          <p>Startups funded</p>
+        </div>
+      </div>
     </div>
+  );
 }
 export default Analytics;
