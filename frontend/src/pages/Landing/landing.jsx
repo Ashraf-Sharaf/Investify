caac6d75af50ecb76f97f728295d85a969056d30
Ashraf Sharaf <ashrafsharafcs@gmail.com>
2024-05-04 19:48:36 +0300

    frontend-creating-footer

--- frontend/src/pages/Landing/landing.jsx
+++ frontend/src/pages/Landing/landing.jsx
@@ -3,13 +3,15 @@ import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Analytics from "./components/Analytics";
 import Stories from "./components/Stories";
+import Footer from "../common/Footer";
 
 function Landing(){
     return <div>
     <Header/>
     <Hero/>
     <Analytics/>
-    <Stories/>
+    {/* <Stories/> */}
+    <Footer/>
     </div>
 }
 export default Landing;
---
+++ frontend/src/pages/common/Footer.jsx
@@ -0,0 +1,39 @@
+import "./footer.css";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import XIcon from '@mui/icons-material/X';
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import EmailIcon from '@mui/icons-material/Email';
+import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
+
+function Footer() {
+  return (
+    <div className="footer flex between center padding-20">
+      <div className="flex column footer-links center gap-10">
+        <img className="logo " src="/images/Investify.png" alt="logo"></img>
+        <div>
+          <InstagramIcon />
+          <FacebookIcon />
+          <LinkedInIcon />
+          <XIcon/>
+        </div>
+      </div>
+      <div className="footer-info flex column gap-10">
+        <div className="flex gap-10">
+          <LocationOnIcon />
+          <p>Beirut, Lebanon</p>
+        </div>
+        <div className="flex gap-10">
+          <LocalPhoneIcon/>
+          <p>+961 010203</p>
+        </div>
+        <div className="flex gap-10">
+          <EmailIcon/>
+          <p>investify@gmail.com</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Footer;
