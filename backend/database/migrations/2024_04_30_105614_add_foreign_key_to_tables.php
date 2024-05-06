b18a31cb2c50b8eea41b85347a9b5a2d097f16ab
Ashraf Sharaf <ashrafsharafcs@gmail.com>
2024-05-06 17:17:07 +0300

    backend-adding-picture-for-user-table

--- backend/database/migrations/2024_04_30_105614_add_foreign_key_to_tables.php
+++ backend/database/migrations/2024_04_30_105614_add_foreign_key_to_tables.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::table('users', function (Blueprint $table) {
             $table->unsignedBigInteger('role_id')->after('password')->nullable();
             $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
+            $table->string('profile_picture')->nullable();
         });
 
         Schema::table('attendees', function (Blueprint $table) {
