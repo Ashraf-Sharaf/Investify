f774091df1ae5b8d839c09b0a782db1fffb7b455
Ashraf Sharaf <ashrafsharafcs@gmail.com>
2024-05-07 14:01:46 +0300

    frontend-adding-investor-pages-routes

---
+++ frontend/src/pages/InvestorRoutes/components/completeProfile.jsx
@@ -0,0 +1,8 @@
+function CompleteProfile() {
+  return (
+    <div>
+      <h1>complete profile</h1>
+    </div>
+  );
+}
+export default CompleteProfile;
--- frontend/src/pages/InvestorRoutes/investor.jsx
+++ frontend/src/pages/InvestorRoutes/investor.jsx
@@ -1,4 +1,13 @@
-function Investor(){
-return <div>investor page</div>
+import { Routes, Route } from "react-router-dom";
+import CompleteProfile from "./components/completeProfile";
+
+function Investor() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<CompleteProfile />} />
+      </Routes>
+    </div>
+  );
 }
 export default Investor;
