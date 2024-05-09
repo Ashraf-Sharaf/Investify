c83a2efe0ff70c10d4746bcfb71c1c67ab2f7ddc
Ashraf Sharaf <ashrafsharafcs@gmail.com>
2024-05-10 01:54:24 +0300

    frontend-editing-right-card-component

--- frontend/src/pages/InvestorRoutes/components/InvestorHome/components/right-card.jsx
+++ frontend/src/pages/InvestorRoutes/components/InvestorHome/components/right-card.jsx
@@ -1,21 +1,19 @@
-function RightCard(){
-    return         <div className="investor-business-card-right flex gap-20 -right ">
-    <div className="investor-business-description-right padding-10 flex column gap-20">
-      <h1>Business Name</h1>
-      <h4>Industry</h4>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-        Explicabo cumque veritatis repellat? Asperiores, enim quisquam eum
-        saepe quod fugit impedit nulla expedita ipsa, odit reprehenderit
-        aut, maiores iure voluptate in?
-      </p>
-      <div className="flex gap-10 center">
-        <button className="investor-business-button-right">View more</button>
+function RightCard({ business }) {
+  const { name, industry, description } = business;
+  return (
+    <div className="investor-business-card-right flex gap-20 -right ">
+      <div className="investor-business-description-right padding-10 flex column gap-20">
+        <h1>{name}</h1>
+        <h4>{industry}</h4>
+        <p>{description}</p>
+
+          <button className="investor-business-button-right">View more</button>
+        
+      </div>
+      <div className="investor-business-img-right">
+        <img src="/images/Investify.png" alt="No image" />
       </div>
     </div>
-    <div className="investor-business-img-right">
-      <img src="/images/Investify.png" alt="No image" />
-    </div>
-  </div>
+  );
 }
 export default RightCard;
