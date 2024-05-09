4efa1dd24bbca8a69fd50065e46fbd7af5fbfa21
Ashraf Sharaf <ashrafsharafcs@gmail.com>
2024-05-10 01:54:42 +0300

    frontend-editing-left-card-component

--- frontend/src/pages/InvestorRoutes/components/InvestorHome/components/left-card.jsx
+++ frontend/src/pages/InvestorRoutes/components/InvestorHome/components/left-card.jsx
@@ -1,22 +1,20 @@
-function LeftBusinesCard(){
-   return <div className="investor-business-card flex gap-20 ">
-    <div className="investor-business-img">
-      <img src="/images/Investify.png" alt="No image" />
-    </div>
-    <div className="investor-business-description padding-10 flex column gap-20">
-      <h1>Business Name</h1>
-      <h4>Industry</h4>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-        Explicabo cumque veritatis repellat? Asperiores, enim quisquam eum
-        saepe quod fugit impedit nulla expedita ipsa, odit reprehenderit
-        aut, maiores iure voluptate in?
-      </p>
-      <div className="flex gap-10 center">
-        <button className="investor-business-button">View more</button>
+function LeftBusinesCard({ business }) {
+  const { name, industry, description } = business;
+
+  return (
+    <div className="investor-business-card flex gap-20 ">
+      <div className="investor-business-img">
+        <img src="/images/Investify.png" alt="No image" />
+      </div>
+      <div className="investor-business-description padding-10 flex column gap-20">
+        <h1>{name}</h1>
+        <h4>{industry}</h4>
+        <p>{description}</p>
+        <div className="flex gap-10 center">
+          <button className="investor-business-button">View more</button>
+          </div>
       </div>
     </div>
-  </div>
-
+  );
 }
 export default LeftBusinesCard;
