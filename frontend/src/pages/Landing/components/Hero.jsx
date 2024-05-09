311660e458d8852afb411dd957ab0304c3fb9c3a
Ashraf Sharaf <ashrafsharafcs@gmail.com>
2024-05-09 18:11:34 +0300

    frontend-editing-landing-page-hero-background

--- frontend/src/pages/Landing/components/Hero.jsx
+++ frontend/src/pages/Landing/components/Hero.jsx
@@ -1,8 +1,9 @@
 function Hero(){
     return <div className="hero-section flex center column ">
-        <h1>Where Entrepreneurs and Investors Connect</h1>
-        <p>Investify is your spot for linking investors with startups. Whether you're an entrepreneur looking for funding or an investor searching for the next great opportunity, Investify makes it simple to find and connect with promising ventures. Join us now and tap into the power of innovation!</p>
 
+            <h1>It's never too late to Invest</h1>
+        <p>Investify is your spot for linking investors with startups. Whether you're an entrepreneur looking for funding or an investor searching for the next great opportunity, Investify makes it simple to find and connect with promising ventures. Join us now and tap into the power of innovation!</p>
+    
     </div>
 }
 export default Hero;
