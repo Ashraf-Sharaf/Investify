ffd8ed9dab67e896d53dfb216d1f488b01495d97
Ashraf Sharaf <ashrafsharafcs@gmail.com>
2024-05-10 19:04:21 +0300

    backend-adding-body-of-the-sent-email-using-blade

--- backend/app/Http/Controllers/EmailController.php
+++ backend/app/Http/Controllers/EmailController.php
@@ -5,12 +5,11 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 use App\Mail\InviteInvestorMail;
+
 class EmailController extends Controller
 {
-    public function send_email(){
-        $to="asha.31@outlook.com";
-        $message="hello DR!";
-
-        Mail::to($to)->send(new InviteInvestorMail($message));
+    public function send_email(Request $request)
+    {
+        Mail::to($request->email)->send(new InviteInvestorMail());
     }
 }
