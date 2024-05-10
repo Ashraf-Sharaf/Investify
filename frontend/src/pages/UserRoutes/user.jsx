32f6c7d401c3d6a6e6734c917e34f67a1e1c80e2
Ashraf Sharaf <ashrafsharafcs@gmail.com>
2024-05-11 01:49:02 +0300

    frontend-adding-navigation-to-user-side-bar

--- frontend/src/pages/UserRoutes/user.jsx
+++ frontend/src/pages/UserRoutes/user.jsx
@@ -1,12 +1,34 @@
 import "./userr.css";
+import HomeIcon from "@mui/icons-material/Home";
+import ComputerIcon from "@mui/icons-material/Computer";
+import EmojiEventsIcon from '@mui/icons-material/EmojiEvents';
+import EventAvailableIcon from '@mui/icons-material/EventAvailable';
+
 function User() {
   return (
     <div className="user-container flex">
-      <div className="user-sidebar flex column padding-20">
-      <div className="landing-nav-logo flex center bottom-border padding-10">
+      <div className="user-sidebar flex column padding-20 gap-20">
+        <div className="landing-nav-logo flex center bottom-border padding-10">
           <img className="logo " src="/images/Investify.png" alt="logo"></img>
         </div>
-      <br/>
+        <div className="user-sidebar-navigations flex column gap-10 ">
+          <div className="flex gap-10 user-sidebar-navigation align" onClick={()=>{}}>
+            <HomeIcon />
+            <h3>Home</h3>
+          </div>
+          <div className="flex gap-10 user-sidebar-navigation align" onClick={()=>{}}>
+            <ComputerIcon />
+            <h3>AI</h3>
+          </div>
+          <div className="flex gap-10 user-sidebar-navigation align " onClick={()=>{}}>
+            <EmojiEventsIcon />
+            <h3>Events</h3>
+          </div>
+          <div className="flex gap-10 user-sidebar-navigation align" onClick={()=>{}}>
+            <EventAvailableIcon/>
+            <h3>Schedule</h3>
+          </div>
+        </div>
       </div>
     </div>
   );
