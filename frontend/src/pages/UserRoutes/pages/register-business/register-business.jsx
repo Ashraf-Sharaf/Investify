b8cae2247851faf607a1b13476fc643225dd208a
Ashraf Sharaf <ashrafsharafcs@gmail.com>
2024-05-12 02:16:20 +0300

    frontend-dding-nav-bar-to-register-business-page

--- frontend/src/pages/UserRoutes/pages/register-business/register-business.jsx
+++ frontend/src/pages/UserRoutes/pages/register-business/register-business.jsx
@@ -1,9 +1,22 @@
-
+import { useNavigate } from "react-router-dom";
 function RegisterBusiness() {
-
+  const navigate = useNavigate();
+  const logout = () => {
+    window.localStorage.setItem("token", null);
+    navigate("/");
+  };
   return (
     <div>
-
+      <div className="investor-nav flex align padding-20 between">
+        <div className="landing-nav-logo ">
+          <img className="logo " src="/images/Investify.png" alt="logo"></img>
+        </div>
+        <div className="flex gap-10">
+          <button className="logout-nav-button" onClick={() => logout()}>
+            Logout
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
