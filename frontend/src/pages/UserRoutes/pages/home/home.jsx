0dfd343fd95825352a0c518aa6935a426840a3a7
Ashraf Sharaf <ashrafsharafcs@gmail.com>
2024-05-12 02:41:44 +0300

    frontend-adding-register-business-form

--- frontend/src/pages/UserRoutes/pages/home/home.jsx
+++ frontend/src/pages/UserRoutes/pages/home/home.jsx
@@ -17,7 +17,7 @@ function Home() {
   }
 
   return (
-    <div className="user-container flex">
+    <div className="flex">
       <div className="user-sidebar flex column padding-20 gap-20">
         <div className="landing-nav-logo flex center bottom-border padding-10">
           <img className="logo " src="/images/Investify.png" alt="logo"></img>
--- frontend/src/pages/UserRoutes/pages/register-business/register-business.jsx
+++ frontend/src/pages/UserRoutes/pages/register-business/register-business.jsx
@@ -1,3 +1,5 @@
+import "./register-business.css";
+import Footer from "../../../Footer/Footer";
 import { useNavigate } from "react-router-dom";
 function RegisterBusiness() {
   const navigate = useNavigate();
@@ -6,7 +8,7 @@ function RegisterBusiness() {
     navigate("/");
   };
   return (
-    <div>
+    <div className="flex column gap-20">
       <div className="investor-nav flex align padding-20 between">
         <div className="landing-nav-logo ">
           <img className="logo " src="/images/Investify.png" alt="logo"></img>
@@ -17,6 +19,50 @@ function RegisterBusiness() {
           </button>
         </div>
       </div>
+
+      <div className="flex center">
+        <div className="register-business-container center flex column around">
+          <h1>Register Your Business</h1>
+          <div className="user-business-info-container flex between padding-10">
+          <div className="flex half-w column gap-20">
+              <div className="user-business-info-input flex between center">
+                <h3>Business Name</h3>
+                <input type="text"></input>
+              </div>
+              <div className="user-business-info-input flex  between center">
+                <h3>Location</h3>
+                <input type="text"></input>
+              </div>
+              <div className="user-business-info-input flex between center">
+                <h3>Industry</h3>
+                <input type="text"></input>
+              </div>
+            </div>
+            <div className="flex half-w column gap-20">
+              <div className="user-business-info-input flex between center">
+              <h3>Funding Needed</h3>
+                <input type="text"></input>
+              </div>
+              <div className="user-business-info-input flex between center">
+                <h3>Stake Offered</h3>
+                <input type="text"></input>
+              </div>
+              <div className="user-business-info-input flex  between center">
+                <h3>Valuation</h3>
+                <input type="text"></input>
+              </div>
+            </div>
+          </div>
+          <div className="flex padding-10 user-business-info-input gap-20 full-w" >
+            <h3>Description</h3>
+            <textarea rows="5"></textarea>
+          </div>
+          <div className=" flex center padding-10">
+          <button className="user-business-button">Submit</button>
+        </div>
+        </div>
+      </div>
+      <Footer />
     </div>
   );
 }
