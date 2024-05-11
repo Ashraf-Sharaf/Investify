f6ce33c11fe04af0b51b91006b78b572d615fe36
Ashraf Sharaf <ashrafsharafcs@gmail.com>
2024-05-12 02:45:21 +0300

    frontend-adding-onclick-event-to-submit-button-in-register-business-form

--- frontend/src/pages/UserRoutes/pages/register-business/register-business.jsx
+++ frontend/src/pages/UserRoutes/pages/register-business/register-business.jsx
@@ -2,11 +2,15 @@ import "./register-business.css";
 import Footer from "../../../Footer/Footer";
 import { useNavigate } from "react-router-dom";
 function RegisterBusiness() {
+
   const navigate = useNavigate();
+
   const logout = () => {
     window.localStorage.setItem("token", null);
     navigate("/");
   };
+
+
   return (
     <div className="flex column gap-20">
       <div className="investor-nav flex align padding-20 between">
@@ -24,7 +28,7 @@ function RegisterBusiness() {
         <div className="register-business-container center flex column around">
           <h1>Register Your Business</h1>
           <div className="user-business-info-container flex between padding-10">
-          <div className="flex half-w column gap-20">
+            <div className="flex half-w column gap-20">
               <div className="user-business-info-input flex between center">
                 <h3>Business Name</h3>
                 <input type="text"></input>
@@ -40,7 +44,7 @@ function RegisterBusiness() {
             </div>
             <div className="flex half-w column gap-20">
               <div className="user-business-info-input flex between center">
-              <h3>Funding Needed</h3>
+                <h3>Funding Needed</h3>
                 <input type="text"></input>
               </div>
               <div className="user-business-info-input flex between center">
@@ -53,13 +57,13 @@ function RegisterBusiness() {
               </div>
             </div>
           </div>
-          <div className="flex padding-10 user-business-info-input gap-20 full-w" >
+          <div className="flex padding-10 user-business-info-input gap-20 full-w">
             <h3>Description</h3>
             <textarea rows="5"></textarea>
           </div>
           <div className=" flex center padding-10">
-          <button className="user-business-button">Submit</button>
-        </div>
+            <button className="user-business-button" onClick={()=>{navigate('/user/home')}}>Submit</button>
+          </div>
         </div>
       </div>
       <Footer />
