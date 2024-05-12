b360faabe67a66b5c7149893089b2b0af376b1b0
Ashraf Sharaf <ashrafsharafcs@gmail.com>
2024-05-13 01:22:08 +0300

    frontend-editing-investor-home-left-card-styling

--- frontend/src/pages/InvestorRoutes/pages/InvestorHome/components/left-card.jsx
+++ frontend/src/pages/InvestorRoutes/pages/InvestorHome/components/left-card.jsx
@@ -3,7 +3,7 @@ import SingleBuisness from "../../SingleBusiness/SingleBusiness";
 function LeftBusinesCard({ business }) {
   const navigate = useNavigate();
 
-  const { id,name, industry, description } = business;
+  const { id, name, industry, description } = business;
   const handleViewMore = () => {
     navigate(`/investor/single-business/${id}`);
   };
@@ -13,14 +13,16 @@ function LeftBusinesCard({ business }) {
         <img src="/images/Investify.png" alt="No image" />
       </div>
       <div className="investor-business-description padding-10 flex column gap-20">
-        <h1>{name}</h1>
-        <h4>{industry}</h4>
-        <p>{description}</p>
-        <div className="flex gap-10 center">
+        <div>
+          <h1>{name}</h1>
+          <h4>{industry}</h4>
+          <p>{description}</p>
+        </div>
+
+        <div className="investor-business-button flex center">
           <button
-            className="investor-business-button"
             onClick={() => {
-              handleViewMore()
+              handleViewMore();
             }}
           >
             View more
