ae0645b7fb494dc44939a2783e1923b0d4b6e7a9
Ashraf Sharaf <ashrafsharafcs@gmail.com>
2024-05-14 23:57:01 +0300

    frontend-adding-routes-to-video-meet

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing/landing";
 import Auth from "./pages/Auth/auth";
+import Video from "./pages/Video/video";
 import UserRoutes from "./pages/UserRoutes/user";
 import AdminRoutes from "./pages/AdminRoutes/admin";
 import InvestorRoutes from "./pages/InvestorRoutes/investor";
 import"./styles/colors.css";
 import"./styles/index.css";
 import"./styles/utilities.css";
+
 
 function App() {
   return (
@@ -15,6 +17,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Landing />} />
           <Route path="/auth" element={<Auth />} />
+          <Route path="/video" element={<Video />} />
           <Route path="/user/*" element={<UserRoutes />} />
           <Route path="/admin/*" element={<AdminRoutes />} />
           <Route path="/investor/*" element={<InvestorRoutes />} />
