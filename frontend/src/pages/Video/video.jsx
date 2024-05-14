7b0d9c5fcec3c090b2911e249dd0be2972c0d15e
Ashraf Sharaf <ashrafsharafcs@gmail.com>
2024-05-14 23:45:54 +0300

    frontend-adding-meeting-view-function

--- frontend/src/pages/Video/video.jsx
+++ frontend/src/pages/Video/video.jsx
@@ -54,7 +54,44 @@ function Video() {
   }
   
   function MeetingView(props) {
-    return null;
+    const [joined, setJoined] = useState(null);
+
+    const { join, participants } = useMeeting({
+     
+      onMeetingJoined: () => {
+        setJoined("JOINED");
+      },
+   
+      onMeetingLeft: () => {
+        props.onMeetingLeave();
+      },
+    });
+    const joinMeeting = () => {
+      setJoined("JOINING");
+      join();
+    };
+  
+    return (
+      <div className="container">
+        <h3>Meeting Id: {props.meetingId}</h3>
+        {joined && joined == "JOINED" ? (
+          <div>
+            <Controls />
+            //For rendering all the participants in the meeting
+            {[...participants.keys()].map((participantId) => (
+              <ParticipantView
+                participantId={participantId}
+                key={participantId}
+              />
+            ))}
+          </div>
+        ) : joined && joined == "JOINING" ? (
+          <p>Joining the meeting...</p>
+        ) : (
+          <button onClick={joinMeeting}>Join</button>
+        )}
+      </div>
+    );
   }
   
   return authToken && meetingId ? (
