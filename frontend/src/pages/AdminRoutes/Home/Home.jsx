d0533b9539a025a35f1d38d6836a4dc29496260c
Ashraf Sharaf <ashrafsharafcs@gmail.com>
2024-05-15 15:37:45 +0300

    frontend-creating-admin-send-invite-form

--- frontend/src/pages/AdminRoutes/Home/Home.jsx
+++ frontend/src/pages/AdminRoutes/Home/Home.jsx
@@ -2,6 +2,8 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import HomeIcon from "@mui/icons-material/Home";
 import LogoutIcon from "@mui/icons-material/Logout";
+import "./home.css";
+
 function Home() {
   const navigate = useNavigate();
   const logout = () => {
@@ -10,7 +12,7 @@ function Home() {
   };
 
   return (
-    <div className="flex">
+    <div className="flex gap-20">
       <div className="user-sidebar admin flex column padding-20 gap-20">
         <div className="landing-nav-logo flex center bottom-border padding-10">
           <img className="logo " src="/images/Investify.png" alt="logo"></img>
@@ -32,6 +34,13 @@ function Home() {
               logout();
             }}
           />
+        </div>
+      </div>
+      <div className="invite-investor-container padding-10 flex column gap-20">
+        <h1>Invite Investor</h1>
+        <div className="invite-investor-inputs flex gap-20 align">
+          <input placeholder="Investor Email"></input>
+          <button>Send Invite</button>
         </div>
       </div>
     </div>
