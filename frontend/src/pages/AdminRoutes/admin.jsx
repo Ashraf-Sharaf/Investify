4c94eb133611230926eeec2500cffb5ad5d178fd
Ashraf Sharaf <ashrafsharafcs@gmail.com>
2024-05-15 14:34:39 +0300

    frontend-editing-admin-routes

--- frontend/src/pages/AdminRoutes/admin.jsx
+++ frontend/src/pages/AdminRoutes/admin.jsx
@@ -1,4 +1,11 @@
-function Admin(){
-return <div>admin page</div>
+import { Routes, Route } from "react-router-dom";
+function Admin() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
 }
 export default Admin;
