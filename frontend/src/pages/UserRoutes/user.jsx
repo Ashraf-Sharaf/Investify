813f23aaae27a98d78319f94645c54d47cebc003
Ashraf Sharaf <ashrafsharafcs@gmail.com>
2024-05-15 18:45:00 +0300

    frontend-editing-user-routes

--- frontend/src/pages/UserRoutes/user.jsx
+++ frontend/src/pages/UserRoutes/user.jsx
@@ -1,15 +1,18 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/home/home";
 import RegisterBusiness from "./pages/register-business/register-business";
-
+import { useState } from "react";
 
 function User() {
+  const [hasBusiness, setHasBusiness] = useState(false);
   return (
     <div>
       <Routes>
-      < Route path="/" element={<RegisterBusiness />} />
+        <Route
+          path="/"
+          element={hasBusiness ? <Navigate to="/user/home" /> : <RegisterBusiness />}
+        />
         <Route path="/home" element={<Home />} />
-  
       </Routes>
     </div>
   );
