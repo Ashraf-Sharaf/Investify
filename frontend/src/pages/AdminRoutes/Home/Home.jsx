cf84e46836f5dde2e502d1e71c14a745fa625b54
Ashraf Sharaf <ashrafsharafcs@gmail.com>
2024-05-15 16:11:02 +0300

    frontend-adding-invite-by-email-function-to-the-admin-dashboard

--- frontend/src/pages/AdminRoutes/Home/Home.jsx
+++ frontend/src/pages/AdminRoutes/Home/Home.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 import HomeIcon from "@mui/icons-material/Home";
 import LogoutIcon from "@mui/icons-material/Logout";
 import "./home.css";
+import { useState } from "react";
 
 function Home() {
   const navigate = useNavigate();
@@ -11,8 +12,29 @@ function Home() {
     navigate("/");
   };
 
+  const [email, setEmail] = useState("");
+  const [error, setError] = useState(null);
+
+  const sendEmail = async () => {
+    try {
+      const data = {
+        email: email,
+      };
+
+      const res = await axios.post(
+        "http://127.0.0.1:8000/api/invite_investor",
+        data,
+        {
+          headers: {
+            Authorization: "Bearer " + window.localStorage.getItem("token"),
+          },
+        }
+      );
+    } catch (error) {}
+  };
+
   return (
-    <div className="flex gap-20">
+    <div className="flex gap-20 relative">
       <div className="user-sidebar admin flex column padding-20 gap-20">
         <div className="landing-nav-logo flex center bottom-border padding-10">
           <img className="logo " src="/images/Investify.png" alt="logo"></img>
@@ -40,9 +62,22 @@ function Home() {
         <h1>Invite Investor</h1>
         <div className="invite-investor-inputs flex gap-20 align">
           <input placeholder="Investor Email"></input>
-          <button>Send Invite</button>
+          <button onClick={() => {sendEmail()}}>Send Invite</button>
         </div>
       </div>
+      {error && (
+        <div className="error-message flex center column gap-20">
+          {error}
+          <button
+            className="error-messge-button"
+            onClick={() => {
+              setError(null);
+            }}
+          >
+            Close
+          </button>
+        </div>
+      )}
     </div>
   );
 }
