d9a089d10b96bdddb4bd245e5090aa42ba7213a2
Ashraf Sharaf <ashrafsharafcs@gmail.com>
2024-05-16 22:12:09 +0300

    frontend-adding-chatbot-user-page

---
+++ frontend/src/pages/UserRoutes/pages/ChatBot/chat_bot.jsx
@@ -0,0 +1,14 @@
+import "../../sidebar.css";
+import { useNavigate } from "react-router-dom";
+import HomeIcon from "@mui/icons-material/Home";
+import ComputerIcon from "@mui/icons-material/Computer";
+import EventAvailableIcon from "@mui/icons-material/EventAvailable";
+import ReviewsIcon from "@mui/icons-material/Reviews";
+import LogoutIcon from "@mui/icons-material/Logout";
+
+function ChatBot(){
+    const navigate = useNavigate();
+
+    return <div></div>
+}
+export default ChatBot;
