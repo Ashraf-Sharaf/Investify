b2dfbb1ad63f83421640d2e29294a3fc47cf5639
Ashraf Sharaf <ashrafsharafcs@gmail.com>
2024-05-16 22:12:47 +0300

    frontend-adding-user-chatbot-page-sidebar

--- frontend/src/pages/UserRoutes/pages/ChatBot/chat_bot.jsx
+++ frontend/src/pages/UserRoutes/pages/ChatBot/chat_bot.jsx
@@ -9,6 +9,52 @@ import LogoutIcon from "@mui/icons-material/Logout";
 function ChatBot(){
     const navigate = useNavigate();
 
-    return <div></div>
+    return <div className="flex">
+    <div className="user-sidebar flex column padding-20 gap-20">
+      <div className="landing-nav-logo flex center bottom-border padding-10">
+        <img className="logo " src="/images/Investify.png" alt="logo"></img>
+      </div>
+      <div className="user-sidebar-navigations flex column gap-10 ">
+        <div
+          className="flex gap-10 user-sidebar-navigation align page-shown"
+          onClick={() => {}}
+        >
+          <HomeIcon />
+          <h3>Home</h3>
+        </div>
+        <div
+          className="flex gap-10 user-sidebar-navigation align"
+          onClick={() => {}}
+        >
+          <ReviewsIcon />
+          <h3>Reviews</h3>
+        </div>
+        <div
+          className="flex gap-10 user-sidebar-navigation align"
+          onClick={() => {}}
+        >
+          <ComputerIcon />
+          <h3>AI</h3>
+        </div>
+        <div
+          className="flex gap-10 user-sidebar-navigation align"
+          onClick={() => {}}
+        >
+          <EventAvailableIcon />
+          <h3>Schedule</h3>
+        </div>
+      </div>
+      <div className="user-sidebar-logout flex between align">
+        <h3>Logout</h3>
+        <LogoutIcon
+          className="logout-icon"
+          onClick={() => {
+            logout();
+          }}
+        />
+      </div>
+    </div>
+    
+    </div>
 }
 export default ChatBot;
