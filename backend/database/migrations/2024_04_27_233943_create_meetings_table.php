25dd1f82d509528bca0a775dfdc837b17be6c4de
Ashraf Sharaf <ashrafsharafcs@gmail.com>
2024-05-16 11:46:18 +0300

    backend-editing-meeting-table

--- backend/database/migrations/2024_04_27_233943_create_meetings_table.php
+++ backend/database/migrations/2024_04_27_233943_create_meetings_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->date('date');
             $table->time('start_time');
-            $table->time('end_time');
+            $table->string('link');
             $table->timestamps();
         });
     }
