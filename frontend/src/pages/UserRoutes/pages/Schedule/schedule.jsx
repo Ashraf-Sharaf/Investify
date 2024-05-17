7bf436dd94d3b52a993d927199f17ad8e035dd40
Ashraf Sharaf <ashrafsharafcs@gmail.com>
2024-05-17 05:05:36 +0300

    frontend-adding-schedule-table-in-schedule-page

--- frontend/src/pages/UserRoutes/pages/Schedule/schedule.jsx
+++ frontend/src/pages/UserRoutes/pages/Schedule/schedule.jsx
@@ -13,7 +13,13 @@ function Schedule() {
     const logout = () => {
         window.localStorage.setItem("token", null);
     }
+    const data = [
+        { col1: 'Row 1 Col 1', col2: 'Row 1 Col 2', col3: 'Row 1 Col 3', col4: 'Row 1 Col 4' },
+        { col1: 'Row 2 Col 1', col2: 'Row 2 Col 2', col3: 'Row 2 Col 3', col4: 'Row 2 Col 4' },
+        { col1: 'Row 3 Col 1', col2: 'Row 3 Col 2', col3: 'Row 3 Col 3', col4: 'Row 3 Col 4' },
+      ];
     
+
   return (
     <div className="flex">
       <div className="user-sidebar flex column padding-20 gap-20">
@@ -62,6 +68,27 @@ function Schedule() {
           />
         </div>
       </div>
+      <div className="schedule-container padding-10 ">    
+      <table>
+      <thead>
+        <tr>
+          <th>Investor Name</th>
+          <th>Date</th>
+          <th>Time</th>
+          <th>Meeting Link</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((row, index) => (
+          <tr key={index}>
+            <td>{row.col1}</td>
+            <td>{row.col2}</td>
+            <td>{row.col3}</td>
+            <td>{row.col4}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table></div>
     </div>
   );
 }
