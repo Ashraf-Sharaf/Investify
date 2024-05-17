e3f4491453fe01b4a40f296a15f8aa47506be127
Ashraf Sharaf <ashrafsharafcs@gmail.com>
2024-05-17 04:52:01 +0300

    frontedn-adding-schedule-page-to-user

---
+++ frontend/src/pages/UserRoutes/pages/Schedule/schedule.jsx
@@ -0,0 +1,53 @@
+function Schedule() {
+  return (
+    <div className="flex">
+      <div className="user-sidebar flex column padding-20 gap-20">
+        <div className="landing-nav-logo flex center bottom-border padding-10">
+          <img className="logo " src="/images/Investify.png" alt="logo"></img>
+        </div>
+        <div className="user-sidebar-navigations flex column gap-10 ">
+          <div
+            className="flex gap-10 user-sidebar-navigation align "
+            onClick={() => {}}
+          >
+            <HomeIcon />
+            <h3>Home</h3>
+          </div>
+          <div
+            className="flex gap-10 user-sidebar-navigation align"
+            onClick={() => {}}
+          >
+            <ReviewsIcon />
+            <h3>Reviews</h3>
+          </div>
+          <div
+            className="flex gap-10 user-sidebar-navigation align"
+            onClick={() => {
+              navigate("/user/chat_bot");
+            }}
+          >
+            <ComputerIcon />
+            <h3>AI</h3>
+          </div>
+          <div
+            className="flex gap-10 user-sidebar-navigation align page-shown"
+            onClick={() => {}}
+          >
+            <EventAvailableIcon />
+            <h3>Schedule</h3>
+          </div>
+        </div>
+        <div className="user-sidebar-logout flex between align">
+          <h3>Logout</h3>
+          <LogoutIcon
+            className="logout-icon"
+            onClick={() => {
+              logout();
+            }}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Schedule;
