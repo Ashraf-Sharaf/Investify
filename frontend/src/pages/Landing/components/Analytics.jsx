169276fa5ba92f79024efdbea85f3eced40d5fb9
Ashraf Sharaf <ashrafsharafcs@gmail.com>
2024-05-18 16:50:29 +0300

    frontend-editing-analytics-in-landing-page

--- frontend/src/pages/Landing/components/Analytics.jsx
+++ frontend/src/pages/Landing/components/Analytics.jsx
@@ -1,8 +1,7 @@
 function Analytics() {
   return (
     <div className="analytics-section flex column around center">
-      <h1 className="padding-10">Quick Facts</h1>
-      <div className="analytics-cards flex around center">
+      <div className="analytics-cards flex between center">
         <div className="analytics-card flex center column around">
           <h1>70+</h1>
           <p>Startups</p>
@@ -20,3 +19,4 @@ function Analytics() {
   );
 }
 export default Analytics;
+ 
