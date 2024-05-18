b8cd5933292f1227b81d367ece07ca3f8dfe1fbf
Ashraf Sharaf <ashrafsharafcs@gmail.com>
2024-05-18 19:44:19 +0300

    frontend-editing-signup-page

--- frontend/src/pages/Auth/components/signup.jsx
+++ frontend/src/pages/Auth/components/signup.jsx
@@ -28,7 +28,7 @@ function Signup({ onToggle }) {
         );
         if (res.status !== 200) {
           setError("Error, try again later!");
-        }else{
+        } else {
           localStorage.setItem("token", res.data.authorisation.token);
           navigate("/user");
         }
@@ -42,59 +42,62 @@ function Signup({ onToggle }) {
   return (
     <div className="flex bg-secondary main-container">
       <div className="flex column center signup greeting-section">
-        <div className="flex center column greeting between">
+        <div className="flex center column greeting around ">
           <h1>Welcome To Investify</h1>
           <p>Register with your personal details to use all of site featuers</p>
+          <img src="/images/signup-img.png" className="login-img"></img>
           <button className="auth-button" onClick={() => onToggle()}>
             Login
           </button>
         </div>
       </div>
       <div className="flex column center auth-section">
-        <div className="flex column between center  auth-form">
+        <div className="flex column between center  auth-form ">
           <h1>Sign Up</h1>
-
-          <input
-            type="email"
-            className="auth-inputs"
-            placeholder="First Name"
-            onChange={(e) => {
-              setFirstname(e.target.value);
-            }}
-            required
-          ></input>
-          <input
-            type="email"
-            className="auth-inputs"
-            placeholder="Last Name"
-            onChange={(e) => {
-              setLastname(e.target.value);
-            }}
-            required
-          ></input>
-          <input
-            className="auth-inputs"
-            placeholder="E-mail"
-            onChange={(e) => {
-              setEmail(e.target.value);
-            }}
-          ></input>
-          <input
-            type="password"
-            className="auth-inputs"
-            placeholder="Password"
-            onChange={(e) => {
-              setPassword(e.target.value);
-            }}
-          ></input>
-          <input
-            type="password"
-            className="auth-inputs"
-            placeholder="Confirm Password"
-            onChange={(e) => {
-              setConfirmPassword(e.target.value);
-            }}
-          ></input>
+          <div className="flex full-w column center gap-10 auth-form-inputs">
+            <input
+              type="email"
+              className="auth-inputs"
+              placeholder="First Name"
+              onChange={(e) => {
+                setFirstname(e.target.value);
+              }}
+              required
+            ></input>
+            <input
+              type="email"
+              className="auth-inputs"
+              placeholder="Last Name"
+              onChange={(e) => {
+                setLastname(e.target.value);
+              }}
+              required
+            ></input>
+            <input
+              className="auth-inputs"
+              placeholder="E-mail"
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+            ></input>
+            <input
+              type="password"
+              className="auth-inputs"
+              placeholder="Password"
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            ></input>
+            <input
+              type="password"
+              className="auth-inputs"
+              placeholder="Confirm Password"
+              onChange={(e) => {
+                setConfirmPassword(e.target.value);
+              }}
+            ></input>
+          </div>
+          <div>
           <button
             className="auth-button"
             onClick={() => {
@@ -103,6 +106,7 @@ function Signup({ onToggle }) {
           >
             Sign Up
           </button>
+          </div>
         </div>
       </div>
       {error && (
